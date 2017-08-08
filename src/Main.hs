000734191ade8c73daea8b6a6f4c940babc03000
Chandra Koduru <kscraja@gmail.com>
2017-08-08 17:14:04 +0530

    working. using options.applicative to handle arguments and options

--- src/Main.hs
+++ src/Main.hs
@@ -3,6 +3,29 @@ module Main where
 import Text.Read (readMaybe)
 import Data.Char (toUpper)
 import System.IO (writeFile)
+import Data.Semigroup ((<>))
+import Options.Applicative (execParser, info, helper, Parser, fullDesc, 
+  progDesc, short, metavar, flag, argument, str, option)
+
+data CommandOptions = CommandOptions
+  { fileName :: FilePath
+  , userName :: String
+  , isUpperCase :: Bool }
+
+upperCaseParser :: Parser Bool
+upperCaseParser = flag False True (short 'u')
+
+fileNameParser :: Parser FilePath
+fileNameParser = argument str (metavar "FILENAME")
+
+userNameParser :: Parser String
+userNameParser = option str (short 'n' <> metavar "USERNAME")
+
+parseOptions :: IO CommandOptions
+parseOptions = execParser $ info (helper <*> commandOptsParser) commandOptsInfo
+  where
+    commandOptsParser = CommandOptions <$> fileNameParser <*> userNameParser <*> upperCaseParser
+    commandOptsInfo = fullDesc <> progDesc "Command line sample program"
 
 getMessage :: IO String
 getMessage = do
@@ -23,23 +46,25 @@ getNumber = do
       getNumber 
     Just i -> return i
 
-runCLI :: IO ()
-runCLI = do
-  let fileName = "myfile.txt"
-  let userName = "John Doe"
-  let isUpperCase = True
+runCLI :: CommandOptions -> IO ()
+runCLI commandOptions = do
+  let file = fileName commandOptions
+  let user = userName commandOptions
+  let uppercase = isUpperCase commandOptions
   message <- getMessage 
   reps <- getRepetitions 
-  writeFile fileName (fileContents userName message reps isUpperCase)
+  writeFile file (fileContents user message reps uppercase)
 
 fileContents :: String -> String -> Int -> Bool -> String
-fileContents userName message repetitions isUpperCase = unlines $ 
-  ("From: " ++ userName) : (replicate repetitions finalMessage)
+fileContents user message repetitions uppercase = unlines $ 
+  ("From: " ++ user) : (replicate repetitions finalMessage)
   where
-    finalMessage = if isUpperCase 
+    finalMessage = if uppercase 
                    then map toUpper message
                    else message
 
 main :: IO ()
-main = runCLI
+main = do
+  options <- parseOptions
+  runCLI options
 
